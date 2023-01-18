1d01ea4d05f1d19c63f7d5b39ca5ab09ec58b864
Darius Rudominer <darius.is.ru@gmail.com>
2023-01-18 14:30:09 -0800

    added framework for chain of checkStructs. Works for test case of a pyramid of 3 materials. Needs to be tested for more cases

--- Assets/Scripts/Checks/checkStructures/CheckStruct.cs
+++ Assets/Scripts/Checks/checkStructures/CheckStruct.cs
@@ -1,14 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CheckStruct
 {
-    CheckStructComponent head;
-    CheckStructComponent curLast;
-  
+    public CheckStructComponent head;
+    public int startLayer;
 
-    public CheckStruct()
+    public CheckStruct(int startLayer, int direction)
     {
+        head = new CheckStructComponent(control.materialType.empty, direction);
+        this.startLayer= startLayer;
+        head.setEndState(true);
     }
+
+    public void append(CheckStructComponent toAppend, CheckStructComponent appendTo, bool holdEndState = false)
+    {
+        appendTo.adjacents.Add(toAppend);
+        toAppend.setEndState(true);
+        if (!holdEndState)
+        {
+            appendTo.setEndState(false);
+        }
+    }
+
+    public void appendAllEnds(CheckStructComponent toAppend, bool holdEndState = false)
+    {
+        appendAllRecurse(toAppend, head, holdEndState);
+    }
+
+    void appendAllRecurse(CheckStructComponent toAppend, CheckStructComponent appendTo, bool holdEndState)
+    {
+        if (appendTo.isEnd())
+        {
+            append(toAppend.clone(), appendTo, holdEndState);
+            toAppend.setEndState(true);
+            return;
+        }
+        foreach(CheckStructComponent curComponent in appendTo.adjacents)
+        {
+            appendAllRecurse(toAppend, curComponent, holdEndState);
+        }
+    }
+
+    public bool satisfy()
+    {
+        CheckStructComponent.satisfyResult startingResult = new CheckStructComponent.satisfyResult();
+        startingResult.startingLayers = new List<int>();
+        startingResult.startingLayers.Add(startLayer);
+        startingResult.direction = head.direction;
+        startingResult.satisfied = true;
+        return satisfyRecurse(head, startingResult, 0);
+    }
+
+    bool satisfyRecurse(CheckStructComponent startingComponent, CheckStructComponent.satisfyResult startingResult, int startingIndex)
+    {
+        CheckStructComponent.satisfyResult curResult = startingComponent.satisfy(startingResult, startingIndex);
+        if (!curResult.satisfied)
+        {
+            return false;
+        }
+
+        if (startingComponent.isEnd())
+        {
+            return true;
+        }
+
+        foreach (CheckStructComponent curComponent in startingComponent.adjacents)
+        {
+            bool any = false;
+            for (int i = 0; i < curResult.startingLayers.Count; i++)
+            {
+                if (satisfyRecurse(curComponent,curResult, i))
+                {
+                    any = true;
+                }
+            }
+            if (!any)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
