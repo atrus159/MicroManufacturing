2f1d53167ffcb9649683b6de81a833f88728b6e3
Darius Rudominer <darius.is.ru@gmail.com>
2023-01-19 13:54:19 -0800

    fixed a bug where empty boxes could not be created at the top since they would go above the top build layer. New bug where rectangles for empty boxes are not generated correctly

--- Assets/Scripts/Checks/checkTwoRectangles.cs
+++ Assets/Scripts/Checks/checkTwoRectangles.cs
@@ -10,12 +10,14 @@ public class checkTwoRectangles : levelRequirementParent
         name = "Create two boxes from gold";
         description = "A box must be between 10 and 20 cells on a side, and no taller than 30 layers.";
         ChromeGoldAlum = new CheckStruct(0, 1);
-        RectangleStructure chromePart = new RectangleStructure(control.materialType.chromium, 1, new Vector3Int(50, 10, 50), new Vector3Int(90, 20, 90), 1, new[] { true, true }, control.materialType.empty);
-        RectangleStructure goldPart = new RectangleStructure(control.materialType.gold, 1, new Vector3Int(20, 10, 20), new Vector3Int(30, 20, 30), 1, new[] { true, true }, control.materialType.empty);
-        RectangleStructure alumPart = new RectangleStructure(control.materialType.aluminum, 1, new Vector3Int(10, 10, 10), new Vector3Int(20, 20, 20), 1, new[] { true, true }, control.materialType.empty);
+        RectangleStructure chromePart = new RectangleStructure(control.materialType.chromium, 1, new Vector3Int(60, 10, 60), new Vector3Int(90, 20, 90), 1, new[] { true, true, false }, control.materialType.empty);
+        RectangleStructure goldPart = new RectangleStructure(control.materialType.gold, 1, new Vector3Int(30, 10, 30), new Vector3Int(60, 20, 60), 1, new[] { true, true, false }, control.materialType.empty); ;
+        RectangleStructure alumPart = new RectangleStructure(control.materialType.aluminum, 1, new Vector3Int(7, 10, 7), new Vector3Int(35, 20, 35), 1, new[] { true, true, false }, control.materialType.empty);
+        RectangleStructure emptyTop = new RectangleStructure(control.materialType.empty, 1, new Vector3Int(90, 1, 90), new Vector3Int(100, 100, 100), 1, new[] { false, false, true });
         ChromeGoldAlum.appendAllEnds(chromePart);
         ChromeGoldAlum.appendAllEnds(goldPart);
         ChromeGoldAlum.appendAllEnds(alumPart);
+        ChromeGoldAlum.appendAllEnds(emptyTop);
     }
 
     public override void check()
